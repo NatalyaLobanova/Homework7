4e2a951e31ed4bb66c680143bec06c672377e679
Natalya Lobanova <knopka40a@mail.ru>
2022-08-31 15:57:29 +0500

    Add Ex052

---
+++ Ex052/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел.
+// Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,7; 5,7; 3,7; 3.
+
+int m = 3;
+int n = 4;
+int[,] matrix = FillMatrix(m, n, 1, 10);
+PrintMatrix(matrix);
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++) 
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        } 
+        Console.WriteLine();   
+    }
+}
+Console.WriteLine();
+
+int[,] FillMatrix(int rowsCount, int columnsCount, int leftRange, int rightRange)
+{
+    int[,] matrix = new int[rowsCount, columnsCount];
+    Random rand = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++) 
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(leftRange, rightRange);
+        }
+    }   
+    return matrix;     
+}
+
+double[] result = new double[n];
+
+for (int j = 0; j < matrix.GetLength(1); j++) 
+{
+    int sum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        sum = sum + matrix[i, j]; 
+        result[j] = sum * 1.0 / m;
+    }
+    Console.Write(result[j] + " ");
+}
